b7ce47a55778131cbba86d179a53480c19599fe9
hwangraehun <86356766+hwangraehun@users.noreply.github.com>
2021-06-24 20:45:24 +0900

    2021.06.24 (숙제)
    for문 이용해서 구구단 29단 1부터 9까지 출력하기
    while문 이용해서 구구단 26단 10부터 18까지 출력하기

---
+++ for.py
@@ -0,0 +1,16 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> for n in range (1,10):
+	print('29','x',n,'=',29*n)
+
+	
+29 x 1 = 29
+29 x 2 = 58
+29 x 3 = 87
+29 x 4 = 116
+29 x 5 = 145
+29 x 6 = 174
+29 x 7 = 203
+29 x 8 = 232
+29 x 9 = 261
+>>> 
---
+++ while.py
@@ -0,0 +1,21 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n=0
+>>> while int(n)<20:
+	for n in range(10,20):
+		print('26','x',n,'=',26*n)
+	if n==19:
+		break
+
+	
+26 x 10 = 260
+26 x 11 = 286
+26 x 12 = 312
+26 x 13 = 338
+26 x 14 = 364
+26 x 15 = 390
+26 x 16 = 416
+26 x 17 = 442
+26 x 18 = 468
+26 x 19 = 494
+>>> 
