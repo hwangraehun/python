986e600acffd9f8d60b9bd8b1a225ff46b923e28
hwangraehun <86356766+hwangraehun@users.noreply.github.com>
2021-10-03 03:57:35 +0900

    Add files via upload

---
+++ 2021.10.2.py
@@ -0,0 +1,59 @@
+# x,y,z='mbc'
+# print(x)
+# print(y)
+# xyz='mbc'
+# print(xyz)
+
+
+# 순서열
+# str
+# list 객체가 모여 객체가 되는것을 list Type
+
+# a=[1,2,3,4,5]
+# del(a[0:2])
+# print(a)
+
+# 튜플
+# list vs tuple
+# []        ()
+# 수정가능 수정 불가능
+# []생략 x  ()생략 o
+
+# a,b=1,2
+# print(a)
+# # range
+
+# 순서열 연산
+
+# 색인(indexing)
+# s=[0,1,2,3,4,5]
+# a=s[3]
+# b=s[-2]
+# print(s[2])
+# print(s[:])
+
+#원소 저장
+# s=[0,1,2,3,4,5]
+# a=len(s) #원소 개수
+# b=min(s) #가장작은 값
+# c=max(s) # 가장 큰값
+# d=sum(s) # 원소 합계
+# e=sum(s,5) #초기값 + ,x값
+
+# print(e)
+# range 적용
+# s= range(1,11,2)
+# a=sum(s)
+# b=sum(s[1:3])
+
+# 원소판단
+# s=(0,1,2,3,4,5)
+# a=5 in s
+# b=5 not in s
+# c='gg' in 'eggs'
+# print(c)
+
+s=[3]
+t=[3,4,6]
+a=s<t
+print(a)
